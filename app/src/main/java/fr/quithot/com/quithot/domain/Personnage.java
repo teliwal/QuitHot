a7cc9440a2a4e470d0d0e5b1f3a2659681184c93
MKostiuk <mathieukostiuk@hotmail.fr>
2018-03-16 13:19:19 +0100

    Correction deplacement persos + on utilise plus les stream java, pas compatibles avec Android 6

--- app/src/main/java/fr/quithot/com/quithot/domain/Personnage.java
+++ app/src/main/java/fr/quithot/com/quithot/domain/Personnage.java
@@ -79,7 +79,7 @@ public class Personnage {
         tempX = x + dirX;
         tempY = y + dirY;
 
-        if ( ! (x < 0.0f && y < 0.0f) && !(x > screenWidth && y > screenHeight)) {
+        if ( ! (x < 0.0f) && !(y < 0.0f) && !(x > screenWidth) && !(y > screenHeight)) {
             x = tempX;
             y = tempY;
         }
